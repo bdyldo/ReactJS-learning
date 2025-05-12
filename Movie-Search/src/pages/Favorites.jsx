94527e0bd86f4dea6faf0fa2fcf68d8892781f4d
bdyldo <dyl123455@outlook.com>
2025-05-12 18:45:58 -0400

    Added Style sheets from web

---
+++ Movie-Search/src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import "../css/Favorites.css"
+
+function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movie Yet</h2>
+      <p>Start adding favorite movies</p>
+    </div>
+  );
+}
+
+export default Favorite;
